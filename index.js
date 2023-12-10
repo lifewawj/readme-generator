9ef207c33568aaf6d5a02e803cf23046e2bb98cf
lifewawj <arevaloaj01@gmail.com>
2023-12-10 15:17:02 -0800

    Questions added to the questions Array to accept user input

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title for your project?'
+    },
+    // DESCRIPTION
+    {
+        type: 'input',
+        name: 'description',
+        messsage: 'Give a brief description of your project?'
+    },
+    // INSTALLATION
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Are there any installation needed for your project?'
+    },
+    // USAGE
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Any instructions for how to use your project?'
+    },
+    // CREDITS
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Any Tutorials, or Third-party assets that you would like to credit?'
+    },
+    // LICENSE
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPL v3', 'Apache 2.0', 'BSD 3-Clause']
+    },
+    // FEATURES
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Does your project contain any features that you would like to indclude?'
+    },
+    // CONTRIBUTION
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can Users contribute to the project?'
+    },
+    // TESTS
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Any Tests that you would to run for this project?'
+    },
+    // USER INFO
+    {
+        type: 'input',
+        name: 'user',
+        message: 'What is your GitHub username?'
+    },
+    // EMAIL
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
