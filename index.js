7c8a89b0592c500dc498d6833ff4777c56a6ef66
lifewawj <arevaloaj01@gmail.com>
2023-12-10 22:57:02 -0800

    ADDED writeToFile Function to write a file with the data recieved from user input, init function to intialize the app, and installed inquirer

--- index.js
+++ index.js
@@ -75,10 +75,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data)), err => {
+        err ? console.log(err) : console.log("Success!")
+    }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile("README.md", data);
+    })
+}
 
 // Function call to initialize app
 init();
